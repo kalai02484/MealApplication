6f434f079aa93b8aaa8eff5a275e0042b6e4e2c4
kalai02484 <kalai02484@gmail.com>
2026-02-17 14:24:20 +0530

    Common api file added

---
+++ src/api/mealApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = "www.themealdb.com/api/json/v1/1";
+
+export const fetchCategories = () => axios.get(`${BASE_URL}/categories.php`);
+
+export const fetchMealsByCategory = (cat) =>
+  axios.get(`${BASE_URL}/filter.php?c=${cat}`);
+
+export const fetchMealById = (id) =>
+  axios.get(`${BASE_URL}/lookup.php?i=${id}`);
+
+export const searchMeals = (query) =>
+  axios.get(`${BASE_URL}/search.php?s=${query}`);
