35c8147477224190e8ac09bbdeff9cb2fa97a46d
kalai02484 <kalai02484@gmail.com>
2026-02-17 13:51:55 +0530

    Navbar Created

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.png";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarToggle,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
+import { FcHome, FcLike } from "react-icons/fc";
 
 const TopNavbar = () => {
   return (
-    <div>
-      TopNavbar comonent
+    <div className="w-full border-b-2 border-amber-500 sticky top-0 bg-white z-50">
+      <Navbar
+        fluid
+        rounded
+        className="mx-auto max-w-7xl"
+      >
+        <NavbarBrand to="/">
+          <img src={logo} className="mr-2 sm:mr-3 h-14 sm:h-18" alt="Logo" />
+        </NavbarBrand>
+        <div className="flex">
+          <NavbarToggle />
+        </div>
+        <NavbarCollapse>
+          <Link to="/"><FcHome className="mr-2 text-xl inline align-bottom"/> Home</Link>
+          <Link to="favorites"><FcLike className="mr-2 text-xl inline align-bottom"/>Favorites</Link>
+        </NavbarCollapse>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
