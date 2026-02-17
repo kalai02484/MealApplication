1e52e2293d57e1a4a474f8b1db9d1137c53f9341
kalai02484 <kalai02484@gmail.com>
2026-02-17 13:43:36 +0530

    Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import MealDetails from "./pages/MealDetails";
+import Favorites from "./pages/Favorites";
+import TopNavbar from "./components/TopNavbar";
 
 const App = () => {
   return (
-    <div>
-      App Component
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <TopNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/meal/:id" element={<MealDetails />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
